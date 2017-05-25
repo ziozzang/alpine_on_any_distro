996a375bdb32ad1fb6650fe7ae4d2130e9f67dde
Jioh L. Jung <ziozzang@gmail.com>
2017-05-25 14:46:36 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 BUSYBOX_BIN_URL="https://www.busybox.net/downloads/binaries/1.26.2-i686/busybox"
 ALPINE_URL="https://nl.alpinelinux.org/alpine/v3.6/releases/x86_64/alpine-minirootfs-3.6.0-x86_64.tar.gz"
 
-TAR_BIN="tar"
+TAR_BIN="/bin2/tar"
 
 # if there's no tar utility, use busybox's tar utility
 if [ ! -f /bin/tar ]; then
@@ -15,4 +15,7 @@ fi
 
 #Fetch Alpine base system
 curl -o /tmp/alpine.tgz ${ALPINE_URL}
-${TAR_BIN} -xvzf /tmp/alpine.tgz --wildcards --no-anchored 'lib' '*apk*' '*busybox'
+mv /bin /bin2
+${TAR_BIN} -xvzf /tmp/alpine.tgz --wildcards --no-anchored 'lib' '*apk*' 'bin'
+apk update
+apk add -f libc6-compat
