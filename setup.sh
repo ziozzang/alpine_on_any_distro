c751ab148b508ad1f35e1266fda166ff5a0c4417
Jioh L. Jung <ziozzang@gmail.com>
2017-05-25 15:33:16 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,19 +3,34 @@
 BUSYBOX_BIN_URL="https://www.busybox.net/downloads/binaries/1.26.2-i686/busybox"
 ALPINE_URL="https://nl.alpinelinux.org/alpine/v3.6/releases/x86_64/alpine-minirootfs-3.6.0-x86_64.tar.gz"
 
-TAR_BIN="/bin2/tar"
+TAR_BIN="/bin/tar"
+
+curl -o /tmp/alpine.tgz ${ALPINE_URL}
 
 # if there's no tar utility, use busybox's tar utility
-if [ ! -f /bin/tar ]; then
+if [ ! -f ${TAR_BIN} ]; then
   curl -o /tmp/busybox ${BUSYBOX_BIN_URL}
   chmod +x /tmp/busybox
-  ln -s /tmp/busybox /tmp/tar
-  TAR_BIN="/tmp/tar"
+  
+  mkdir -p /tmp/alpine
+  cd /tmp/alpine
+  /tmp/busybox tar -xzvf /tmp/alpine.tgz
+  mv -f lib/* /lib/
+  mv -f sbin/apk /sbin/
+  mv -f usr/share/apk /usr/share/
+  mv -f etc/apk /etc/
+  mv -f var/cache/apk /var/cache/
+  mv -f var/lib/apk /var/lib/
+  mv -f bin/busybox /bin/
+  #cp bin/* /bin/
+  #cp sbin/* /sbin/
+  cd /
+  #rm -rf /tmp/alpine*
+else
+  #Fetch Alpine base system
+  ${TAR_BIN} -xvzf /tmp/alpine.tgz --wildcards --no-anchored 'lib' '*apk*' 'bin'
+
 fi
 
-#Fetch Alpine base system
-curl -o /tmp/alpine.tgz ${ALPINE_URL}
-mv /bin /bin2
-${TAR_BIN} -xvzf /tmp/alpine.tgz --wildcards --no-anchored 'lib' '*apk*' 'bin'
 apk update
-apk add -f libc6-compat
+#apk add -f libc6-compat
